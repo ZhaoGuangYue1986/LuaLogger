26e146a9a5f62e4facebfabf0412f835a8acb33f
ZhaoGuangYue1986 <jeky_zhao@.com>
2020-03-01 11:00:45 +0800

    First version for log

---
+++ Log.lua
@@ -0,0 +1,57 @@
+---
+-- Created by zhao guangyue (jeky_zhao@qq.com)
+-- DateTime: 2020/3/1 9:45
+-- Copyright (c) 2015 ostack. http://www.ostack.cn
+-- This source code is licensed under BSD 3-Clause License (the "License").
+-- You may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     https://opensource.org/licenses/BSD-3-Clause
+--/
+
+local Log = { _l_log_curr_level_ = 1,
+              _l_log_level_debug = 0,
+              _l_log_level_info = 1,
+              _l_log_level_warn = 2,
+              _l_log_level_error = 3,
+              _l_log_level_call_back_ = nil,
+              setLogCallBack = setLogCallBack,
+              setLogLevel = setLogLevel,
+              debug = debug,
+              info = info,
+              warn = warn,
+              error = error,
+}
+
+function Log:setLogCallBack(callback)
+    self._l_log_level_call_back_ = callback
+end
+
+function Log:setLogLevel(level)
+    self._l_log_curr_level_ = level
+end
+
+function Log:debug(msg)
+    self:print(self._l_log_level_debug, "[DEBUG]", msg)
+end
+function Log:info(msg)
+    self:print(self._l_log_level_info, "[INFO]", msg)
+end
+function Log:warn(msg)
+    self:print(self._l_log_level_info, "[WARN]", msg)
+end
+function Log:error(msg)
+    self:print(self._l_log_level_info, "[ERROR]", msg)
+end
+
+function Log:print(level, prefix, msg)
+    if self._l_log_curr_level_ <= level then
+        if nil ~= self._l_log_level_call_back_ then
+            self._l_log_level_call_back_(self._l_log_level_info, msg)
+        else
+            print(prefix .. " " .. msg)
+        end
+    end
+end
+
+return Log;
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # LuaLogger
 一个超简单的LUA 日志工具（A very simple Lua Logger tool）
+## 使用方法（How to use）
+使用前需要require 本模块，通过返回的变量来调用提供的方法
+Should require Log first, then use the return val's function
+##使用示例
+local Log = require("LuaLogger.Log")
+###直接调用Log的方法，（use defual log print function）
+Log:debug("this msg is teat use log");
+Log:info("this msg is teat use log");
+Log:warn("this msg is teat use log");
+Log:error("this msg is teat use log");
+###设置自己的log日志方法（set self log fuction）
+local function log_callBack(level, msg)
+    print(level .. msg)
+end
+Log:setLogCallBack(log_callBack)
+Log:info("this msg is teat use self define log call back");
+###修改日志级别，默认打印info以上级别（Set log level，default is info）
+Log:setLogLevel(Log._l_log_level_error);
